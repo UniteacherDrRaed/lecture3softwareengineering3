777540fcd24100550e7c4d8f68a52e3d6a01701e
Dr.Raed <drraed.jb@gmail.com>
2024-04-02 12:24:34 +0300

    page41

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -33,32 +33,13 @@ class MainPage extends StatelessWidget {
         elevation: 0,
         titleTextStyle: const TextStyle(color: Colors.lime),
       ),
-      body: Container(
-        margin:const EdgeInsets.all(25),
-        padding: const EdgeInsets.all(25),
-        alignment: Alignment.bottomCenter,
-        decoration:  BoxDecoration(
-          gradient: SweepGradient(
-            startAngle: pi,
-            endAngle: 2*pi,
-            colors: [
-              Colors.red,
-              Colors.yellow,
-              Colors.green,
-            ]
-          ),
-          boxShadow:const [
-            BoxShadow(
-              color:Colors.lime,
-              offset: Offset(10,16),
-              blurRadius: 5,
-            ),
-          ],
-          borderRadius: BorderRadius.circular(25),
-        ),
-        width: 200,
-        height: 250,
-        child: const Text("Software Engineering 3"),
+      body: IconButton(
+        onPressed: (){
+         ScaffoldMessenger.of(context).showSnackBar(
+           const SnackBar(content: Text("example snackbar"))
+         );
+        },
+        icon: Icon(Icons.add),
       ),
     );
   }
@@ -244,3 +225,73 @@ class MainPage extends StatelessWidget {
     );
   }
 }*/
+
+
+/*
+page 37
+import 'package:flutter/material.dart';
+import 'dart:math';
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example"),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
+        leading: const Icon(
+          Icons.umbrella,
+          color: Colors.white,
+          size: 21,
+        ),
+        leadingWidth: 100,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.tv,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.radio,
+                color: Colors.white,
+              )),
+        ],
+        elevation: 0,
+        titleTextStyle: const TextStyle(color: Colors.lime),
+      ),
+      body: Container(
+        margin:const EdgeInsets.all(25),
+        padding: const EdgeInsets.all(25),
+        alignment: Alignment.bottomCenter,
+        decoration:  BoxDecoration(
+          gradient: SweepGradient(
+            startAngle: pi,
+            endAngle: 2*pi,
+            colors: [
+              Colors.red,
+              Colors.yellow,
+              Colors.green,
+            ]
+          ),
+          boxShadow:const [
+            BoxShadow(
+              color:Colors.lime,
+              offset: Offset(10,16),
+              blurRadius: 5,
+            ),
+          ],
+          borderRadius: BorderRadius.circular(25),
+        ),
+        width: 200,
+        height: 250,
+        child: const Text("Software Engineering 3"),
+      ),
+    );
+  }
+}
+*/
