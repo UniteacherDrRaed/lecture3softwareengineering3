0e4d38541b54ad1e6dfa095cfa962f563576f13e
Dr.Raed <drraed.jb@gmail.com>
2024-04-02 11:53:11 +0300

    page18

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -12,11 +12,15 @@ class MainPage extends StatelessWidget {
         leading: const Icon(Icons.umbrella,
           color: Colors.white,
           size: 21,),
+        leadingWidth: 100,
         actions: [
           IconButton(onPressed: (){}, icon:const Icon(Icons.tv,color: Colors.white,)),
           IconButton(onPressed: (){}, icon:const  Icon(Icons.radio,color: Colors.white,)),
         ],
+        elevation: 0,
+       titleTextStyle: const TextStyle(color: Colors.lime),
       ),
+      body: Container(color:Colors.lightGreen),
     );
   }
 }
@@ -36,6 +40,39 @@ class MainPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.pink,
         leading: const Icon(Icons.umbrella,color: Colors.white,size: 21,),
+      ),
+    );
+  }
+}
+
+*/
+
+
+
+/*
+page 14
+
+import 'package:flutter/material.dart';
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example"),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
+        leading: const Icon(Icons.umbrella,
+          color: Colors.white,
+          size: 21,),
+        leadingWidth: 100,
+        actions: [
+          IconButton(onPressed: (){}, icon:const Icon(Icons.tv,color: Colors.white,)),
+          IconButton(onPressed: (){}, icon:const  Icon(Icons.radio,color: Colors.white,)),
+        ],
+        elevation: 40,
+        shadowColor: Colors.red,
       ),
     );
   }
