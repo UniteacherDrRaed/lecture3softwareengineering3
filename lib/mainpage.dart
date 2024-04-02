59951032d8dd8ec22380498bda8056baf88b4738
Dr.Raed <drraed.jb@gmail.com>
2024-04-02 11:48:50 +0300

    page11

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -9,8 +9,36 @@ class MainPage extends StatelessWidget {
         title: const Text("Example"),
         centerTitle: true,
         backgroundColor: Colors.pink,
+        leading: const Icon(Icons.umbrella,
+          color: Colors.white,
+          size: 21,),
+        actions: [
+          IconButton(onPressed: (){}, icon:const Icon(Icons.tv,color: Colors.white,)),
+          IconButton(onPressed: (){}, icon:const  Icon(Icons.radio,color: Colors.white,)),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*
+page 10
+import 'package:flutter/material.dart';
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example"),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
         leading: const Icon(Icons.umbrella,color: Colors.white,size: 21,),
       ),
     );
   }
 }
+
+*/
