0ebfa84e4d82e12dfd42e76f6a8aeb77caeb0906
Dr.Raed <drraed.jb@gmail.com>
2024-04-02 11:44:06 +0300

    page10

---
+++ android/app/src/main/kotlin/com/example/lecture_4_software_engineering/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lecture_4_software_engineering
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/mainpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example"),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
+        leading: const Icon(Icons.umbrella,color: Colors.white,size: 21,),
+      ),
+    );
+  }
+}
